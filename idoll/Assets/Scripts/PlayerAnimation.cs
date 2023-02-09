813106478c7a6afdc9461352d446699ff274a078
AlexanderC5 <91765309+AlexanderC5@users.noreply.github.com>
2023-02-09 11:39:59 -0800

    Player Animation Organizing
    Waiting on a few variables from playerMovement

--- idoll/Assets/Scripts/PlayerAnimation.cs
+++ idoll/Assets/Scripts/PlayerAnimation.cs
@@ -4,27 +4,22 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
-    public Animator animator; //drag in "PlayerSprite(Animator)"
+    [Tooltip("Drag in 'PlayerSprite (Animator)'")]
+    public Animator animator;
 
     private void SpriteAnimation()
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
 
-
         //send value to anitatior parameters
         animator.SetFloat("xAxis", movement.x);
         animator.SetFloat("yAxis", movement.y);
         animator.SetFloat("magnitude", movement.magnitude);
-
-
-
     }
 
-    // Update is called once per frame
     void Update()
     {
         SpriteAnimation();
-
     }
 
 }
