334c4a16d43e8d857a87d2cd0e9f37610438e146
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2023-02-26 22:50:10 -0800

    Add very barebones scene change function to Game Manager

--- idoll/Assets/Scripts/GameManager.cs
+++ idoll/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // References: 
 // https://medium.com/nerd-for-tech/implementing-a-game-manager-using-the-singleton-pattern-unity-eb614b9b1a74
@@ -39,14 +40,21 @@ public class GameManager : MonoBehaviour
     }
     #endregion
 
+    #region Properties
     // Call and change GameMode using Yarn Spinner functions
     // GameMode is a variable of enum type, defined in enums.cs
     public GameMode GameMode { get; set; }
 
-    // Get story FSM..?
+    // Get story state... also idk we need to maintain bools somewhere...
     public StoryState StoryState { get; set; }
 
-    // List all GameObjects
+    #endregion
+
+    // Public function to change scenem can be called from anywhere w/ access to GameManager
+    public void ChangeToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
 
     // MAYBE Player + Inventory
 
