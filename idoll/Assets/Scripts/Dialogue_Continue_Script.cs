2dc9441c5e23701c5da5f7e3fe5392b9fe60f686
AlexanderC5 <91765309+AlexanderC5@users.noreply.github.com>
2023-04-22 16:51:50 -0700

    Updated dialogue - custom line script will not auto-skip text

--- idoll/Assets/Scripts/Dialogue_Continue_Script.cs
+++ idoll/Assets/Scripts/Dialogue_Continue_Script.cs
@@ -2,25 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Yarn.Unity;
+using UnityEngine.InputSystem; // New Unity input system!
 
 public class Dialogue_Continue_Script : MonoBehaviour
 {
-    private float timer;
-    private float duration = 0.5f; //Add delay between skips
-    [SerializeField] LineView lineview;
+    private Inputs input;
 
-    private void Start()
+    private float timer;
+    private float duration = 0.1f; // Add delay between skips
+    [SerializeField] LineViewCustom lineview;
+
+    #region InputSystem
+
+    private void Awake()
     {
-        timer = 0.5f;
+        input = new Inputs();
+    }
+
+    private void OnEnable()
+    {
+        input.Enable();
+        input.Player.Primary.performed += OnInteract;
+    }
+
+    private void OnDisable()
+    {
+        input.Disable();
+        input.Player.Primary.performed -= OnInteract;
+    }
+
+    #endregion
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        if (timer <= 0)
+        {
+            lineview.OnContinueClicked();
+            timer = duration;
+        }
     }
 
     // Update is called once per frame
     private void Update()
     {
-        if(Input.GetButton("Interact") && timer == 0) {
-            lineview.OnContinueClicked();
-            timer = duration;
-        }
         if (timer > 0)
         {
             timer = Mathf.Max(0, timer - Time.deltaTime);
